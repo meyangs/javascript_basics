17a03240fcfe9cd6e52cfa5ed5df2c7ebf634900
Meyangz <israelvictor126@gmail.com>
2023-02-26 14:23:01 +0100

    ctd-logical.js

--- comparison.js
+++ comparison.js
@@ -1,33 +1,9 @@
 var x = 5;
 var y = 6;
 
-if (x == y) {
-  console.log("x == y", x == y);
-} else {
-  console.log("x == y", x == y);
-}
-if (x !== y) {
-  console.log("x !== y", x !== y);
-} else {
-  console.log("x !== y", x !== y);
-}
-if (x > y) {
-  console.log("x > y", x > y);
-} else {
-  console.log("x > y", x > y);
-}
-if (x >= y) {
-  console.log("x >= y", x >= y);
-} else {
-  console.log("x >= y", x >= y);
-}
-if (x <= y) {
-  console.log("x <= y", x <= y);
-} else {
-  console.log("x <= y", x <= y);
-}
-if (x < y) {
-  console.log("x < y", x < y);
-} else {
-  console.log("x < y", x < y);
-}
+console.log(x, "==", y, x == y);
+console.log(x, "!==", y, x !== y);
+console.log(x, ">", y, x > y);
+console.log(x, ">=", y, x >= y);
+console.log(x, "<=", y, x <= y);
+console.log(x, "<", y, x < y);
--- logical.js
+++ logical.js
@@ -1,13 +1,7 @@
 var x = 5;
 var y = 6;
 
-if (x > 50 && y < 100) {
-  console.log("x > 50 && y < 100", x > 50 && y < 100);
-} else {
-  console.log("x > 50 && y < 100", x > 50 && y < 100);
-}
-if (x > 50 || y < 100) {
-  console.log("x > 50 || y < 100", x > 50 || y < 100);
-} else {
-  console.log("x > 50 || y < 100", x > 50 || y < 100);
-}
+console.log(x, "> 50 && ", y, " < 100", x > 50 && y < 100);
+console.log(x, "> 50 && ", y, "< 100", x > 50 && y < 100);
+console.log(x, "> 50 || ", y, " < 100", x > 50 || y < 100);
+console.log(x, "> 50 || ", y, " < 100", x > 50 || y < 100);
